cced39c9febcb43d30fd9d01dfd2a6415693a922
Cypphi <altcypphi@gmail.com>
2022-03-03 21:17:41 +0100

    will stop comitting after this to not flood gh actions

--- src/main/java/hazeclient/haze/HazeClient.java
+++ src/main/java/hazeclient/haze/HazeClient.java
@@ -5,14 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HazeClient implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	// Client meta data (looks familiar right?... XD)
+	public static String name = "Haze Client", version = "0.0.1", BUILDID = "1";
+
+	public static final Logger LOG = LoggerFactory.getLogger(name);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOG.info("Initializing " + name + " " + version + "_" + BUILDID + "...");
 	}
 }
