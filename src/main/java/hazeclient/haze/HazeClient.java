417b09a7dc10c9000426f5a77a6816c576bc28fb
Cypphi <altcypphi@gmail.com>
2022-03-03 20:58:11 +0100

    fabric example mod gaming

---
+++ src/main/java/hazeclient/haze/HazeClient.java
@@ -0,0 +1,18 @@
+package hazeclient.haze;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HazeClient implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
