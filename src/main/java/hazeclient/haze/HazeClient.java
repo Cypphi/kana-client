954759d74ed474cee2ceda964b0be7183a62c982
Cypphi <altcypphi@gmail.com>
2022-03-04 02:10:29 +0100

    Some stuff

--- src/main/java/hazeclient/haze/HazeClient.java
+++ src/main/java/hazeclient/haze/HazeClient.java
@@ -1,17 +1,34 @@
 package hazeclient.haze;
 
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 public class HazeClient implements ModInitializer {
 	// Client meta data (looks familiar right?... XD)
 	public static String name = "Haze Client", version = "0.0.1", BUILDID = "1";
-
+	public static final MinecraftClient mc = MinecraftClient.getInstance();
 	public static final Logger LOG = LoggerFactory.getLogger(name);
+
+	private static File hazeClientPath = new File("haze-client");
 
 	@Override
 	public void onInitialize() {
 		LOG.info("Initializing " + name + " " + version + "_" + BUILDID + "...");
+
+		try {
+			if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+				hazeClientPath.mkdirs();
+			} else {
+				LOG.info("This is a client-side utility mod you dumb nigger.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
