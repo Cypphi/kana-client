c46f339bd3f098d0e360aafd8d4c54adcf8d6253
Cypphi <altcypphi@gmail.com>
2022-03-04 06:36:48 +0100

    anti retard precautions

---
+++ src/main/java/hazeclient/haze/Main.java
@@ -0,0 +1,10 @@
+package hazeclient.haze;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Dumbass, put the mod in your mods folder.");
+    }
+}
