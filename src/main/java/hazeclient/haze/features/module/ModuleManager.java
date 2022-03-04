d38e5e57a243377dce5f138f55ceb8645ceda2b4
sigmaclientwastaken <blockmbot.2@gmail.com>
2022-03-04 20:06:38 +0100

    state of the art shitcode part 2

--- src/main/java/hazeclient/haze/features/module/ModuleManager.java
+++ src/main/java/hazeclient/haze/features/module/ModuleManager.java
@@ -1,4 +1,27 @@
 package hazeclient.haze.features.module;
 
+import hazeclient.haze.features.module.impl.misc.*;
+
+import java.util.ArrayList;
+
 public class ModuleManager {
+
+    private final ArrayList<Module> modules = new ArrayList<>();
+
+    public ModuleManager() {
+        initialize();
+    }
+
+    protected void initialize() {
+
+        modules.clear();
+
+        modules.add(new TestModule());
+
+    }
+
+    public ArrayList<Module> getModules() {
+        return modules;
+    }
+
 }
---
+++ src/main/java/hazeclient/haze/features/module/impl/misc/TestModule.java
@@ -0,0 +1,12 @@
+package hazeclient.haze.features.module.impl.misc;
+
+import hazeclient.haze.features.module.Category;
+import hazeclient.haze.features.module.Module;
+
+public class TestModule extends Module {
+
+    public TestModule() {
+        super(Category.MOVEMENT, "TestModule", "Experimental Module");
+    }
+
+}
