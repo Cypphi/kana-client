ef3465cddfb24c0ec4494f6e2eba750f3c716295
Cypphi <altcypphi@gmail.com>
2022-03-04 18:42:38 +0100

    KeyboardMixin :weary:

---
+++ src/main/java/hazeclient/haze/mixins/KeyboardMixin.java
@@ -0,0 +1,15 @@
+package hazeclient.haze.mixins;
+
+import net.minecraft.client.Keyboard;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Keyboard.class)
+public abstract class KeyboardMixin {
+    @Inject(method = "onKey", at = @At("HEAD"), cancellable = true)
+    public void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo info) {
+        System.out.println("Successfully pressed key: " + key);
+    }
+}
