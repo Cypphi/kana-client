6a663e8a1b8bf9a6afb72195792d632b344dad4c
Cypphi <altcypphi@gmail.com>
2022-03-06 15:20:33 +0100

    w

--- src/main/java/hazeclient/haze/mixins/KeyboardMixin.java
+++ src/main/java/hazeclient/haze/mixins/KeyboardMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Keyboard.class)
 public abstract class KeyboardMixin {
     @Inject(method = "onKey", at = @At("HEAD"), cancellable = true)
-    public void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo info) {
+    public void onKeyEvent_1(long window, int key, int scancode, int action, int modifiers, CallbackInfo info) {
 
     }
 }
