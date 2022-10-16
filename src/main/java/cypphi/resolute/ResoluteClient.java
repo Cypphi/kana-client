90dca7e1e09ff51ba9bd266677bc9affbf56dd2f
Cypphi <altcypphi@gmail.com>
2022-10-17 01:01:15 +0200

    forgor 💀

--- src/main/java/cypphi/resolute/ResoluteClient.java
+++ src/main/java/cypphi/resolute/ResoluteClient.java
@@ -1,13 +1,11 @@
-package hazeclient.haze;
+package cypphi.resolute;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-
-public class HazeClient implements ModInitializer {
+public class ResoluteClient implements ModInitializer {
 	public static String name = "Haze", version = "0.0.1";
 	public static final MinecraftClient mc = MinecraftClient.getInstance();
 	public static final Logger LOGGER = LoggerFactory.getLogger(name);
