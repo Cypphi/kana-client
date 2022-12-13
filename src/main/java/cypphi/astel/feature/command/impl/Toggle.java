5583d04c312d204ae4165392e584a111c4576bc5
unknown <lto44675@gmail.com>
2022-12-13 11:46:42 +0100

    fixes

--- src/main/java/cypphi/astel/feature/command/impl/Toggle.java
+++ src/main/java/cypphi/astel/feature/command/impl/Toggle.java
@@ -1,16 +1,9 @@
 package cypphi.astel.feature.command.impl;
 
-<<<<<<< HEAD:src/main/java/cypphi/amiss/feature/command/impl/Toggle.java
-import cypphi.amiss.AmissClient;
-import cypphi.amiss.feature.command.Command;
-import cypphi.amiss.feature.module.Module;
-import cypphi.amiss.feature.module.ModuleManager;
-=======
 import cypphi.astel.AstelClient;
 import cypphi.astel.feature.command.Command;
 import cypphi.astel.feature.module.ModuleManager;
 import cypphi.astel.feature.module.Module;
->>>>>>> e5e48951a161c0a4d93710f533cca640511c1a13:src/main/java/cypphi/astel/feature/command/impl/Toggle.java
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
