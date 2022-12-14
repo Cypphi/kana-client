617e5907839b68c484d5a2ca15c9a8988747c6ef
cypphi <altcypphi@gmail.com>
2022-12-14 19:22:25 +0100

    h

--- src/main/java/cypphi/astel/AstelClient.java
+++ src/main/java/cypphi/astel/AstelClient.java
@@ -8,8 +8,7 @@ import org.slf4j.LoggerFactory;
 public class AstelClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("Astel Client");
     public static final MinecraftClient mc = MinecraftClient.getInstance();
-    public static String    name = "Astel Client",
-                            version = "1.0.0";
+    public static String name = "Astel Client", version = "1.0.0";
 
     @Override
     public void onInitializeClient() {
